a2a8234c76f9083ce6624808ad49f3ab53465539
Nikita Istomin <nikita.istomin54@gmail.com>
2023-07-02 02:39:25 -0500

    Inproved Navbar

--- project/components/header.tsx
+++ project/components/header.tsx
@@ -1,30 +1,35 @@
-import Image from "next/image";
 import Link from "next/link";
-import Logo from "../public/design/logo.svg";
 
-const Header = () => {
+const Navbar = () => {
     return (
-        <div className='flex h-20 w-full flex-row items-center justify-between px-5 shadow-md '>
-            <Link href={"/"} className='h-full object-contain py-3'>
-                <Image src={Logo} alt='Home' className=' h-full w-auto' />
-            </Link>
-            <ul className='flex w-auto flex-row items-center gap-x-5'>
-                <li className=' p-3'>
-                    <a href='#about'>About</a>
-                </li>
-                <li className=' p-3'>
-                    <a href='#projects'>Projects</a>
-                </li>
-                <li>
-                    <Link
-                        className='rounded-full border-2 border-yellow-600 bg-gradient-to-r from-yellow-500 to-orange-400 p-3'
-                        href='/connect'>
-                        Get in touch
-                    </Link>
-                </li>
-            </ul>
-        </div>
+        <nav className=' h-20 w-full bg-transparent'>
+            <div className='mx-auto flex h-full max-w-7xl flex-row items-center justify-between px-5'>
+                <Link
+                    href={"/"}
+                    className='h-full object-contain py-3 transition-transform hover:scale-105 '>
+                    <svg
+                        className='h-full w-auto fill-nier-700'
+                        width='392'
+                        height='598'
+                        viewBox='0 0 392 598'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M57 491.5L0 448L0.5 149L56.5 106L168.5 220.5V21.5L196.5 0L224.5 21.5V278L335.5 391V106L391.5 149V448.5L335 492L224.5 378.5V576.5L196 598L168.5 576.5V321L57 206.5V491.5Z' />
+                    </svg>
+                </Link>
+                <ul className='flex w-auto flex-row items-center gap-x-5'>
+                    <li className=' p-3 transition hover:scale-105'>
+                        <a href='#about'>About</a>
+                    </li>
+                    <li className=' p-3 transition hover:scale-105'>
+                        <a href='#projects'>Projects</a>
+                    </li>
+                    <li className='rounded-full border-2 border-nier-700 bg-nier-300 p-3 transition duration-300 ease-in-out hover:scale-105 hover:bg-nier-400'>
+                        <Link href='/connect'>Get in touch</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
-export default Header;
+export default Navbar;
