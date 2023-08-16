36f84c4a2b4b9fa99cbcb7be7a4373da6e11b50d
Nikita Istomin <nikita.istomin54@gmail.com>
2023-08-16 04:08:05 -0500

    Global State Provider set up

--- project/app/_globalComponents/JotaiProvider.tsx
+++ project/app/_globalComponents/JotaiProvider.tsx
@@ -3,6 +3,8 @@
 import { Provider } from "jotai";
 import { ReactNode } from "react";
 
-export default function JotaiProvider({ children }: { children: ReactNode }) {
+const JotaiProvider = ({ children }: { children: ReactNode }) => {
   return <Provider>{children}</Provider>;
-}
+};
+
+export default JotaiProvider;
