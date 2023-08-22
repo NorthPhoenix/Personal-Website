8fe08c986c5828b15ed185a31e86aafdfb76b4d5
Nikita Istomin <nikita.istomin54@gmail.com>
2023-08-22 18:05:37 -0500

    Set up S3, MongoDB, Prisma

---
+++ project/lib/s3.ts
@@ -0,0 +1,29 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const bucketName = process.env.AWS_BUCKET_NAME as string;
+const region = process.env.AWS_BUCKET_REGION as string;
+const accessKeyId = process.env.AWS_ACCESS_KEY as string;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY as string;
+
+const s3Client = new S3Client({
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
+
+export async function getObjectSignedUrl(key: string) {
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+  };
+
+  // https://aws.amazon.com/blogs/developer/generate-presigned-url-modular-aws-sdk-javascript/
+  const command = new GetObjectCommand(params);
+  const seconds = 3600;
+  const url = await getSignedUrl(s3Client, command, { expiresIn: seconds });
+
+  return url;
+}
