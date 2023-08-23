88e58fe84f1b10569167f734686649ddde5078ea
Nikita Istomin <nikita.istomin54@gmail.com>
2023-08-22 20:13:59 -0500

    Build fix atempt

--- project/lib/s3.ts
+++ project/lib/s3.ts
@@ -3,7 +3,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const bucketName = process.env.AWS_BUCKET as string;
 const region = process.env.AWS_REGION as string;
-const accessKeyId = process.env.AWS_ACCESS_KEY as string;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID as string;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY as string;
 
 const s3Client = new S3Client({
