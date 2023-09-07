82948cfd3c115470087c6e2fda4f7ebe85c77244
Nikita Istomin <nikita.istomin54@gmail.com>
2023-09-07 02:21:20 -0500

    Formated Education section for all screen sizes

--- app/home/_localComponents/educationSection/CreditsCounter.tsx
+++ app/home/_localComponents/educationSection/CreditsCounter.tsx
@@ -4,9 +4,13 @@ import { animate } from "framer-motion"
 
 type CreditsCounterProps = {
   credits: number
+  counterSide?: "left" | "right"
 }
 
-const CreditsCounter = ({ credits }: CreditsCounterProps) => {
+const CreditsCounter = ({
+  credits,
+  counterSide = "right",
+}: CreditsCounterProps) => {
   const progress = 0
   const barRef = useRef<HTMLDivElement>(null)
   const creditsRef = useRef<HTMLSpanElement>(null)
@@ -25,17 +29,30 @@ const CreditsCounter = ({ credits }: CreditsCounterProps) => {
       },
     })
   }, [])
-  return (
-    <div className='flex flex-row items-center justify-stretch py-2 pl-4 pr-2'>
+  return counterSide === "right" ? (
+    <div className='flex flex-row items-center justify-stretch px-2 py-2 md:px-4'>
       <div className=' relative m-2 h-1 grow rounded-full bg-utd-100 bg-opacity-30'>
         <div
           className='absolute left-0 top-0 h-full rounded-full bg-utd-100'
           ref={barRef}
         />
       </div>
-      <div className='mr-2 box-content flex w-20 grow-0 flex-row items-center justify-end gap-2 p-2 text-base '>
+      <div className='w-18 box-content flex grow-0 flex-row items-center justify-end gap-2 p-2 text-sm md:w-20 md:text-base '>
         <span ref={creditsRef}>0</span>
         <span>credits</span>
+      </div>
+    </div>
+  ) : (
+    <div className='flex flex-row items-center justify-stretch px-2 py-2 md:px-4'>
+      <div className='w-18 box-content flex grow-0 flex-row items-center justify-end gap-2 p-2 text-sm md:w-20 md:text-base '>
+        <span ref={creditsRef}>0</span>
+        <span>credits</span>
+      </div>
+      <div className=' relative m-2 h-1 grow rounded-full bg-utd-100 bg-opacity-30'>
+        <div
+          className='absolute left-0 top-0 h-full rounded-full bg-utd-100'
+          ref={barRef}
+        />
       </div>
     </div>
   )
--- app/home/_localComponents/educationSection/Education.tsx
+++ app/home/_localComponents/educationSection/Education.tsx
@@ -26,7 +26,8 @@ const Education = () => {
       </div>
       {/* Content */}
       <div className='w-full max-w-7xl'>
-        <div className='nier-block-left-sm md:nier-block-left relative my-0 ml-4 mr-2 px-0 py-2 sm:m-4 sm:p-2 md:p-8'>
+        {/* Screen size - sm+ */}
+        <div className='nier-block-left-sm md:nier-block-left relative my-0 ml-4 mr-2 hidden px-0 py-2 sm:m-4 sm:block sm:p-2 md:p-8'>
           <article className=' flex flex-col items-stretch justify-start bg-nier-200 shadow-xl'>
             <div className='-mb-4 flex flex-row items-center justify-between'>
               <Link
@@ -39,28 +40,66 @@ const Education = () => {
                   height={512}
                   src='/images/utd-logo.png'
                   alt='UTD Logo'
-                  className='ml-4 box-content aspect-square h-24 w-auto p-2'
+                  className='ml-3 box-content aspect-square h-16 w-auto p-1 md:ml-4 md:h-20 md:p-2 lg:h-24'
                 />
-                <div className='ml-2 mt-2 flex flex-col items-start justify-start gap-1 p-2'>
-                  <span className='text-shadow text-xl font-bold tracking-wide transition-colors duration-150 group-hover:text-amber-700'>
+                <div className='ml-1 mt-1 flex flex-col items-start justify-start gap-1 p-2 md:ml-2 md:mt-2'>
+                  <span className='text-shadow text-base font-bold tracking-wide transition-colors duration-150 group-hover:text-amber-700 lg:text-xl'>
                     The University of Texas at Dallas
                   </span>
-                  <div className='flex flex-col items-start justify-start font-semibold leading-4 opacity-60'>
+                  <div className='flex flex-col items-start justify-start text-xs font-semibold leading-5 opacity-60 md:text-sm lg:text-base'>
                     <span>Bachelor of Science in Computer Science</span>
                     <span>GPA: 3.66</span>
                   </div>
                 </div>
               </Link>
-              <div className='mr-4 flex flex-col items-end justify-start gap-1 p-2'>
+              <div className='mb-4 mr-3 flex flex-col items-end justify-start gap-1 p-2 text-xs md:mb-3 md:mr-4 md:text-sm lg:text-base'>
+                <span className='font-semibold opacity-80'>
+                  Spring Class of 2023
+                </span>
                 <span className=' font-semibold opacity-60'>
                   Richardson, TX, USA
                 </span>
-                <span className='font-semibold opacity-80'>
-                  Graduated Spring 2023
-                </span>
               </div>
             </div>
-            <CreditsCounter credits={135} />
+            <CreditsCounter credits={135} counterSide='right' />
+          </article>
+        </div>
+        {/* Screen size - xs */}
+        <div className='nier-block-left-sm relative my-0 ml-4 mr-2 px-0 py-2 sm:hidden '>
+          <article className=' flex flex-col items-stretch justify-start bg-nier-200 shadow-xl'>
+            <div className='-mb-4'>
+              <Link
+                prefetch={false}
+                target='_blank'
+                href='https://www.utdallas.edu/'
+                className='group flex flex-row items-center justify-start'>
+                <Image
+                  width={512}
+                  height={512}
+                  src='/images/utd-logo.png'
+                  alt='UTD Logo'
+                  className='ml-2 box-content aspect-square h-12 w-auto p-1 '
+                />
+                <div className='flex flex-col items-start justify-start gap-1 p-2'>
+                  <span className='text-shadow text-base font-bold tracking-wide transition-colors duration-150 group-hover:text-amber-700'>
+                    The University of Texas at Dallas
+                  </span>
+                  <div className='flex flex-col items-start justify-start text-xs font-semibold leading-4 opacity-60 '>
+                    <span>Bachelor of Science in Computer Science</span>
+                    <span>GPA: 3.66</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <CreditsCounter credits={135} counterSide='right' />
+            <div className='-mt-4 mb-1 flex flex-row items-center justify-between px-4 py-2 text-xs '>
+              <span className='font-semibold opacity-80'>
+                Spring Class of 2023
+              </span>
+              <span className=' font-semibold opacity-60'>
+                Richardson, TX, USA
+              </span>
+            </div>
           </article>
         </div>
       </div>
