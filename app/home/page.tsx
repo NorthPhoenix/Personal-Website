b2ac83bd06a970b8f8e60c56adc36cf10abe8fde
Nikita Istomin <nikita.istomin54@gmail.com>
2023-09-09 17:26:36 -0500

    Backround patterns - Correct position of background patterns & added more

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -17,8 +17,22 @@ const Home = () => {
       <Navbar />
       <Hero />
       <div className='relative overflow-hidden'>
-        <LinesThroughCircleDesign className='absolute left-0 top-0 -z-20 h-[90vw] -translate-x-[40%] -translate-y-48 fill-none stroke-nier-400 opacity-60' />
-        <LinesThroughCircleDesign className='absolute right-0 top-[800px] -z-20 h-[90vw] -translate-y-[400px] translate-x-[40%] rotate-180 fill-none stroke-nier-400 opacity-60' />
+        <div className='absolute left-0 right-0 top-[5%] -z-20 h-[100vw] lg:top-0'>
+          <LinesThroughCircleDesign className='absolute -top-[10%] left-0 h-[90vw] -translate-x-[40%] fill-none stroke-nier-400 opacity-60' />
+          <LinesThroughCircleDesign className='absolute right-0 top-[40%] h-[90vw] translate-x-[40%] rotate-180 fill-none stroke-nier-400 opacity-60' />
+        </div>
+        <div className='absolute left-0 right-0 top-[25%] -z-20 h-[100vw] sm:top-[20%] md:top-[22%] lg:top-[20%] xl:top-[33%]'>
+          <LinesThroughCircleDesign className='absolute -top-[10%] left-0 h-[90vw] -translate-x-[40%] fill-none stroke-nier-400 opacity-60' />
+          <LinesThroughCircleDesign className='absolute right-0 top-[40%] h-[90vw] translate-x-[40%] rotate-180 fill-none stroke-nier-400 opacity-60' />
+        </div>
+        <div className='absolute left-0 right-0 top-[45%] -z-20 h-[100vw] sm:top-[40%] md:top-[44%] lg:top-[40%] xl:bottom-[5%] xl:top-[unset]'>
+          <LinesThroughCircleDesign className='absolute -top-[10%] left-0 h-[90vw] -translate-x-[40%] fill-none stroke-nier-400 opacity-60' />
+          <LinesThroughCircleDesign className='absolute right-0 top-[40%] h-[90vw] translate-x-[40%] rotate-180 fill-none stroke-nier-400 opacity-60' />
+        </div>
+        <div className='absolute bottom-[10%] left-0 right-0 -z-20 h-[100vw] sm:bottom-[7%] md:bottom-[8%] lg:bottom-[7%] xl:hidden'>
+          <LinesThroughCircleDesign className='absolute -top-[10%] left-0 h-[90vw] -translate-x-[40%] fill-none stroke-nier-400 opacity-60' />
+          <LinesThroughCircleDesign className='absolute right-0 top-[40%] h-[90vw] translate-x-[40%] rotate-180 fill-none stroke-nier-400 opacity-60' />
+        </div>
         <DiamondTransition
           twTransitionFill='fill-black'
           twTransitionStroke='stroke-nier-700'
@@ -29,14 +43,14 @@ const Home = () => {
         <Skills />
         <Projects />
         <Education />
+        <DiamondTransition
+          twTransitionFill='fill-black'
+          twTransitionStroke='stroke-nier-700'
+          unitWidth={150}
+          padding={"50px"}
+          reverse
+        />
       </div>
-      <DiamondTransition
-        twTransitionFill='fill-black'
-        twTransitionStroke='stroke-nier-700'
-        unitWidth={150}
-        padding={"50px"}
-        reverse
-      />
       <div className='flex min-h-screen flex-col justify-between bg-black '>
         <ContactMe className='' />
         <Footer />
