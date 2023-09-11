82e885464bfaf74ee8861c92bff56433c017ef87
Nikita Istomin <nikita.istomin54@gmail.com>
2023-09-10 20:21:54 -0500

    Added tech description to the footer

--- app/home/_localComponents/Footer.tsx
+++ app/home/_localComponents/Footer.tsx
@@ -11,12 +11,15 @@ const Footer = () => {
           <Logo className='h-8 w-8' />
           <span className='ml-3 text-xl'>Nikita Istomin</span>
         </a>
-        <div className=' mt-4 flex flex-col items-center justify-center sm:ml-4 sm:mt-0 sm:items-start sm:border-l-2 sm:border-nier-400'>
-          <p className='text-sm sm:pl-4'>
-            2023 — Powered by my remaining brain cells :D
+        <div className=' mt-4 flex flex-col items-center justify-center opacity-70 sm:ml-4 sm:mt-0 sm:items-start sm:border-l-2 sm:border-nier-400'>
+          <p className='text-sm sm:pl-4 sm:text-xs md:text-sm'>
+            2023 — Powered by my remaining brain cells :D (and Vercel)
+          </p>
+          <p className='text-sm sm:pl-4 sm:text-xs md:text-sm'>
+            Created from scratch with Next.js, TailwindCSS, and TypeScript
           </p>
           <a
-            className='text-sm sm:pl-4'
+            className='text-sm sm:pl-4 sm:text-xs md:text-sm'
             href='https://www.flaticon.com/free-icons/drag-and-drop'
             title='drag and drop icons'>
             Drag and drop icons created by bsd - Flaticon
