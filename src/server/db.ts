4d560df2c7d448fc90793236d2a9d51bdc4613d9
Nikita Istomin <nikita.istomin54@gmail.com>
2024-02-20 04:31:47 -0600

    refactor: Clean dead code

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,12 +2,7 @@ import { createClient } from "@libsql/client"
 import { env } from "~/env.mjs"
 
 export default createClient({
-  url:
-    process.env.NODE_ENV == "production" ?
-      env.DB_URL ?? "http://localhost:8080"
-    : "file:db.sqlite",
+  url: process.env.NODE_ENV == "production" ? env.DB_URL : "file:db.sqlite",
   authToken:
-    process.env.NODE_ENV == "production" ?
-      env.DB_AUTH_TOKEN ?? undefined
-    : undefined,
+    process.env.NODE_ENV == "production" ? env.DB_AUTH_TOKEN : undefined,
 })
