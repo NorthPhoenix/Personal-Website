de336d9c175a35ede9c62ec24f93e7be628d282c
Nikita Istomin <nikita.istomin54@gmail.com>
2024-02-20 04:38:25 -0600

    fix: redirect url fix

--- src/server/auth_providers/github.ts
+++ src/server/auth_providers/github.ts
@@ -3,11 +3,11 @@ import { env } from "~/env.mjs"
 
 export function createGitHubOAuthProvider(redirect_uri?: string) {
   const redirectURL =
-    process.env.NODE_ENV === "production" ?
-      !!process.env.VERCEL_URL ?
-        "https://" + process.env.VERCEL_URL
-      : "http://localhost:3000"
-    : "http://localhost:3000" + "/api/login_callback/github" + redirect_uri
+    (process.env.NODE_ENV === "production" ?
+      "https://nikitaistomin.com"
+    : "http://localhost:3000") +
+    "/api/login_callback/github" +
+    redirect_uri
   console.log("redirectURL", redirectURL)
   return new GitHub(env.AUTH_GITHUB_CLIENT_ID, env.AUTH_GITHUB_CLIENT_SECRET, {
     redirectURI: redirectURL,
