d670c4d2b8de077da08991ac3f863b6fecb49883
Nikita Istomin <nikita.istomin54@gmail.com>
2024-02-27 03:38:28 -0600

    refacor: env vars

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,7 +2,7 @@ import { createClient } from "@libsql/client"
 import { env } from "~/env.mjs"
 
 export default createClient({
-  url: process.env.NODE_ENV == "production" ? env.DB_URL : "file:db.sqlite",
+  url: env.DB_URL,
   authToken:
     process.env.NODE_ENV == "production" ? env.DB_AUTH_TOKEN : undefined,
 })
